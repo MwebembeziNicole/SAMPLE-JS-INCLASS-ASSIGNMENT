3b728a9684336f72a355da87a428e3a8fb5ff6c2
MwebembeziNicole <nicolemwebembezi18@gmail.com>
2025-02-10 19:46:12 +0300

    asunc

---
+++ asynch_and_await.js
@@ -0,0 +1,14 @@
+export const fetchUsingAsynchAwait = async (url) => {
+    try{
+        const res = await fetch(URL);
+        if (!response.ok){
+            throw new Error(`Error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data.slice(0,7);
+    }
+    catch(error){
+        throw new Error(error);
+    }
+
+}
--- promises.js
+++ promises.js
@@ -1,11 +1,11 @@
 export const fetchUsingPromise = (url) => {
     return new Promise((resolve, reject) => {
         fetch(URL)
-        .then(resp => {
+        .then(response => {
             if (Ires.ok) {
                 throw new Error('Error! Status: ${res.status}');
             }
-            return resp.json();
+            return response.json();
         })
         .then(data => {
             resolve(data.slice(0,7));
